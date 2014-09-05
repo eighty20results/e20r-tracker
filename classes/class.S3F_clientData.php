7fa148dc094e7a3d41082f3bb1d8ea7b6fc37f60
Thomas Sjolshagen <thomas@eighty20results.com>
2014-09-05 06:57:36 -0400

    Added menu page creation functions

--- classes/class.S3F_clientData.php
+++ classes/class.S3F_clientData.php
@@ -32,12 +32,42 @@ class S3F_clientData {
         // TODO: Create page for back-end to display customers.
     }
 
+    /* TODO: Create the client pages for the menu */
+    public function e20r_client_pages() {
+
+    }
+
+    public function client_assignments_page() {
+
+    }
+
+    public function client_measurements_page() {
+
+    }
+
+    public function client_habits_page() {
+
+    }
+
+    public function client_meals_page() {
+
+    }
+
+    /**
+     *  Add the datepicker scripts for the admin pages (TODO: Figure out how to use datepicker JS in Wordpress
+     */
+    public function admin_scripts() {
+
+    }
+
     public function initAdmin() {
         $page = add_menu_page('Client Tracker', __('Client Tracker','e20r_tracker'), 'manage_options',  'e20r-tracker', array(&$this,'e20r_client_pages'),'div');
-        add_submenu_page('s3fclients', __('Assignments','e20r_tracker'), __('Assignments','e20r_tracker'), 'manage-optison', "e20r-tracker-data", array(&$this,'client_assignments_page'));
+        add_submenu_page('s3fclients', __('Assignments','e20r_tracker'), __('Assignments','e20r_tracker'), 'manage-options', "e20r-tracker-data", array(&$this,'client_assignments_page'));
         add_submenu_page('s3fclients', __('Measurements','e20r_tracker'), __('Measurements','e20r_tracker'), 'manage-options', "e20r-tracker-data", array(&$this,'client_measurements_page'));
         add_submenu_page('s3fclients', __('Habits','e20r_tracker'), __('Habits','e20r_tracker'), 'manage_options', "e20r-tracker-data", array(&$this,'client_habits_page'));
         add_submenu_page('s3fclients', __('Meals','e20r_tracker'), __('Meal History','e20r_tracker'), 'manage_options', "e20r-tracker-data", array(&$this,'client_meals_page'));
+
+        /* TODO: add_action( "admin_print_scripts-$page", array( &$this, 'admin_scripts' ) ); */
 /*
   add_submenu_page('appointments', __('Shortcodes','e20r_tracker'), __('Shortcodes','e20r_tracker'), App_Roles::get_capability('manage_options', App_Roles::CTX_PAGE_SHORTCODES), "app_shortcodes", array(&$this,'shortcodes_page'));
         add_submenu_page('appointments', __('FAQ','e20r_tracker'), __('FAQ','e20r_tracker'), App_Roles::get_capability('manage_options', App_Roles::CTX_PAGE_FAQ), "app_faq", array(&$this,'faq_page'));
