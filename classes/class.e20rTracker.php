ac7fbc077ff6bf1c8e02917575c5d8a70498fa99
Thomas Sjolshagen <Thomas@eighty20results.com>
2014-09-25 18:34:52 -0400

    Moved plugin activiation & deactivation scripts.

--- classes/class.e20rTracker.php
+++ classes/class.e20rTracker.php
@@ -23,8 +23,8 @@ class e20rTracker {
         $this->clientData = new S3F_clientData();
 
         dbg("Register activation/deactiviation hooks");
-        register_activation_hook( E20R_PLUGIN_DIR, array( &$this, 'activateE20R_Plugin' ) );
-        register_deactivation_hook( E20R_PLUGIN_DIR, array( &$this, 'deactivateE20R_Plugin' ) );
+        register_activation_hook( E20R_PLUGIN_DIR, 'activateE20R_Plugin' );
+        register_deactivation_hook( E20R_PLUGIN_DIR, 'deactivateE20R_Plugin' );
 
         dbg("Added action to load Admin Page");
         add_action( 'admin_menu', array( &$this, 'loadAdminPage') );
@@ -80,8 +80,6 @@ class e20rTracker {
      */
     public function load_adminJS()
     {
-        dbg("Loading scripts and stuff for admin pages");
-
         wp_register_script('e20r_tracker_admin', E20R_PLUGINS_URL . '/js/e20r-tracker-admin.js', array('jquery'), '0.1', true);
 
         /* Localize ajax script */
@@ -113,6 +111,18 @@ class e20rTracker {
 
     }
 
+    public function load_JSscript() {
+
+        wp_register_script('e20r_tracker_js', E20R_PLUGINS_URL . '/js/e20r-tracker.js', array('jquery'), '0.1', true);
+
+        wp_localize_script('e20r_tracker_js', 'e20r_tracker',
+            array(
+                'ajaxurl' => admin_url( 'admin-ajax.php' ),
+            )
+        );
+
+        wp_enqueue_script('e20r_tracker_js');
+    }
     /* Load graphing scripts */
     public function load_plotSW() {
 
@@ -146,125 +156,6 @@ class e20rTracker {
         wp_deregister_style( 'jqplot' );
         wp_enqueue_style( 'jqplot', E20R_PLUGINS_URL . '/js/jQPlot/core/jquery.jqplot.min.css', false, '0.1' );
 
-    }
-
-    public function activateE20R_Plugin() {
-
-        global $wpdb;
-        global $e20r_db_version;
-
-        $charset_collate = '';
-
-        if ( ! empty( $wpdb->charset ) ) {
-            $charset_collate = "DEFAULT CHARACTER SET {$wpdb->charset}";
-        }
-
-        if ( ! empty( $wpdb->collate ) ) {
-            $charset_collate .= " COLLATE {$wpdb->collate}";
-        }
-
-
-        $intakeTableSql =
-            "CREATE TABLE If NOT EXISTS {$this->tables->client_info} (
-                id int not null auto_increment,
-                user_id int not null,
-                user_dob date not null,
-                height decimal(18, 3) null,
-                heritage int null,
-                waist_circumference decimal(18,3),
-                weight decimal(18,3) null,
-                is_metric tinyint default 0,
-                is_imperial tinyint default 0,
-                is_gb_imperial tinyint default 0,
-                use_pictures tinyint default 0,
-                for_research tinyint default 0,
-                chronic_pain tinyint default 0,
-                injuries tinyint default 0,
-                primary_key (id),
-                key user_id (user_id asc) )
-              {$charset_collate}
-            ";
-
-        $measurementTableSql =
-            "CREATE TABLE IF NOT EXISTS {$this->tables->measurements} (
-                id int not null auto_increment,
-                user_id int not null,
-                recorded_date datetime null,
-                weight decimal(18,3) null,
-                neck decimal(18,3) null,
-                shoulder decimal(18,3) null,
-                chest decimal(18,3) null,
-                arm decimal(18,3) null,
-                waist decimal(18,3) null,
-                hip decimal(18,3) null,
-                thigh decimal(18,3) null,
-                calf decimal(18,3) null,
-                girth decimal(18,3) null,
-                primary key  (id),
-                key user_id ( user_id asc) )
-              {$charset_collate}
-          ";
-
-        $itemsTableSql =
-            "CREATE TABLE IF NOT EXISTS {$this->tables->checkin_items} (
-                id int not null auto_increment,
-                short_name varchar(20) null,
-                program_id int null,
-                item_name varchar(50) null,
-                startdate datetime null,
-                enddate datetime null,
-                item_order int not null default 1,
-                maxcount int null,
-                membership_level_id int not null default 0,
-            primary key  (id) ,
-            unique key shortname_UNIQUE (short_name asc) )
-            {$charset_collate}";
-
-        $businessRulesSql =
-            "CREATE TABLE IF NOT EXISTS {$this->tables->checkin_rules} (
-                id int not null auto_increment,
-                checkin_id int null,
-                success_rule mediumtext null,
-                primary key  (id),
-                key checkin_id (checkin_id asc) )
-            {$charset_collate}";
-
-        $checkinSql =
-            "CREATE TABLE IF NOT EXISTS {$this->tables->checkin} (
-                id int not null auto_increment,
-                user_id int null,
-                checkin_date datetime null,
-                checkin_id int null,
-                program_id int null, -- Uses the membership_level->ID value (unless it's nourish)
-                primary key  (id) )
-            {$charset_collate}";
-
-        require_once( ABSPATH . "wp-admin/includes/upgrade.php" );
-
-        dbg('dbInstall() - Creating tables in database');
-        dbDelta( $itemsTableSql );
-        dbDelta( $businessRulesSql );
-        dbDelta( $checkinSql );
-        dbDelta( $measurementTableSql );
-        dbDelta( $intakeTableSql );
-
-        add_option( 'e20rTracker_db_version', $e20r_db_version );
-
-        flush_rewrite_rules();
-    }
-
-    public function deactivateE20R_Plugin() {
-
-        global $wpdb;
-        global $e20r_db_version;
-
-        foreach ( $this->tables as $tblName ) {
-
-            dbg("dbUninstall() - {$tblName} being dropped");
-
-            $sql = "DROP TABLE IF EXISTS {$tblName}";
-            $wpdb->query( $sql );
-        }
     }
 
 } 
--- e20r-tracker.php
+++ e20r-tracker.php
@@ -82,6 +82,138 @@ if ( ! function_exists( 'dbg' ) ):
 
 endif;
 
+if ( ! function_exists( 'activateE20R_Plugin') ):
+    function activateE20R_Plugin() {
+
+        global $wpdb;
+        global $e20r_db_version;
+
+        $charset_collate = '';
+
+        if ( ! empty( $wpdb->charset ) ) {
+            $charset_collate = "DEFAULT CHARACTER SET {$wpdb->charset}";
+        }
+
+        if ( ! empty( $wpdb->collate ) ) {
+            $charset_collate .= " COLLATE {$wpdb->collate}";
+        }
+
+        dbg("Loading table SQL");
+
+        $intakeTableSql =
+            "CREATE TABLE If NOT EXISTS {$this->tables->client_info} (
+                    id int not null auto_increment,
+                    user_id int not null,
+                    user_dob date not null,
+                    height decimal(18, 3) null,
+                    heritage int null,
+                    waist_circumference decimal(18,3),
+                    weight decimal(18,3) null,
+                    is_metric tinyint default 0,
+                    is_imperial tinyint default 0,
+                    is_gb_imperial tinyint default 0,
+                    use_pictures tinyint default 0,
+                    for_research tinyint default 0,
+                    chronic_pain tinyint default 0,
+                    injuries tinyint default 0,
+                    primary_key (id),
+                    key user_id (user_id asc) )
+                  {$charset_collate}
+                ";
+
+        $measurementTableSql =
+            "CREATE TABLE IF NOT EXISTS {$this->tables->measurements} (
+                    id int not null auto_increment,
+                    user_id int not null,
+                    recorded_date datetime null,
+                    weight decimal(18,3) null,
+                    neck decimal(18,3) null,
+                    shoulder decimal(18,3) null,
+                    chest decimal(18,3) null,
+                    arm decimal(18,3) null,
+                    waist decimal(18,3) null,
+                    hip decimal(18,3) null,
+                    thigh decimal(18,3) null,
+                    calf decimal(18,3) null,
+                    girth decimal(18,3) null,
+                    primary key  (id),
+                    key user_id ( user_id asc) )
+                  {$charset_collate}
+              ";
+
+        $itemsTableSql =
+            "CREATE TABLE IF NOT EXISTS {$this->tables->checkin_items} (
+                    id int not null auto_increment,
+                    short_name varchar(20) null,
+                    program_id int null,
+                    item_name varchar(50) null,
+                    startdate datetime null,
+                    enddate datetime null,
+                    item_order int not null default 1,
+                    maxcount int null,
+                    membership_level_id int not null default 0,
+                primary key  (id) ,
+                unique key shortname_UNIQUE (short_name asc) )
+                {$charset_collate}";
+
+        $businessRulesSql =
+            "CREATE TABLE IF NOT EXISTS {$this->tables->checkin_rules} (
+                    id int not null auto_increment,
+                    checkin_id int null,
+                    success_rule mediumtext null,
+                    primary key  (id),
+                    key checkin_id (checkin_id asc) )
+                {$charset_collate}";
+
+        $checkinSql =
+            "CREATE TABLE IF NOT EXISTS {$this->tables->checkin} (
+                    id int not null auto_increment,
+                    user_id int null,
+                    checkin_date datetime null,
+                    checkin_id int null,
+                    program_id int null, -- Uses the membership_level->ID value (unless it's nourish)
+                    primary key  (id) )
+                {$charset_collate}";
+
+        require_once( ABSPATH . "wp-admin/includes/upgrade.php" );
+
+        dbg('dbInstall() - Creating tables in database');
+        dbDelta( $itemsTableSql );
+        dbDelta( $businessRulesSql );
+        dbDelta( $checkinSql );
+        dbDelta( $measurementTableSql );
+        dbDelta( $intakeTableSql );
+
+        add_option( 'e20rTracker_db_version', $e20r_db_version );
+
+        flush_rewrite_rules();
+    }
+endif;
+
+if ( ! function_exists( 'deactivateE20R_Plugin' ) ):
+
+function deactivateE20R_Plugin() {
+
+    global $wpdb;
+    global $e20r_db_version;
+
+    $deleteTables = get_option( 'e20r_clean_tables', false );
+
+    dbg("Loading table SQL");
+
+    if ( $deleteTables !== false) {
+
+        foreach ( $this->tables as $tblName ) {
+
+            dbg( "dbUninstall() - {$tblName} being dropped" );
+
+            $sql = "DROP TABLE IF EXISTS {$tblName}";
+            $wpdb->query( $sql );
+        }
+    }
+}
+endif;
+
 $e20rTracker = new e20rTracker();
 $e20rTracker->init();
 
