0c9c10ccc03060bdd5956bafe96ade6c62a6f0b4
Thomas Sjolshagen <Thomas@eighty20results.com>
2014-10-01 14:37:53 -0400

    Feature: Manage all Check-In items from the same page (Add/Edit/Delete). Still have Javascript & AJAX support to add.

--- classes/class.E20Rcheckin.php
+++ classes/class.E20Rcheckin.php
@@ -106,68 +106,184 @@ if ( ! class_exists( 'E20Rcheckin' ) ):
 
             $programs = new e20rPrograms();
 
-            if ( ! empty($item) ) {
-
-                $start = new DateTime( $item->startdate );
-                $end = new DateTime( $item->enddate );
-
-                $diff = $end->diff( $start );
-
-                $max = $diff->format( '%a' );
-            }
-            else {
-                $max = 14; // Default max if a habit is 14 days.
-            }
-
             ob_start();
             ?>
             <H1>List of Check-In/Activity Items</H1>
             <hr />
-            <div id="edit-checkin-items">
-                <?php if ( count($item_list) > 0) { ?>
-                    <form action="" method="post">
-                    <?php wp_nonce_field('e20r-tracker-data', 'e20r_tracker_edit_checkins'); ?>
-                    <div class="e20r-checkin-editform">
-                        <input type="hidden" name="hidden_e20r_checkin_item_id" id="hidden_e20r_checkin_item_id" value="<?php echo ( ( ! empty($item) ) ? $item->id : 0 ); ?>">
-                        <table id="e20r-manage-checkin-items">
-                            <thead>
-                                <tr>
-                                    <td class="e20r-label header">Program</td>
-                                    <td class="e20r-label header"><label for="e20r-checkin-short_name">Short name:</label></td>
-                                    <td class="e20r-label header"><label for="e20r-checkin-item_name">Name:</label></td>
-                                    <td class="e20r-label header"><label for="e20r-checkin-startdate">Starts on:</label></td>
-                                    <td class="e20r-label header"><label for="e20r-checkin-enddate">Ends on:</label></td>
-                                    <td class="e20r-label header"><label for="e20r-checkin-item_order">Sort order:</label></td>
-                                    <td class="e20r-label header"><label for="e20r-checkin-maxcount">Max # of check-ins:</label></td>
+            <form action="" method="post">
+                <?php wp_nonce_field('e20r-tracker-data', 'e20r_tracker_edit_checkins'); ?>
+                <div class="e20r-checkin-editform">
+                    <table id="e20r-manage-checkin-items">
+                        <thead>
+                            <tr>
+                                <th class="e20r-label header"><label for="checkin-item-edit">Edit</label></th>
+                                <th class="e20r-label header"><label for="e20r-checkin-item-id">Id</label></th>
+                                <th class="e20r-label header"><label for="e20r-checkin-item-order">Order #</label></th>
+                                <th class="e20r-label header"><label for="e20r-program">Program</label></th>
+                                <th class="e20r-label header"><label for="e20r-checkin-short-name">Short name</label></th>
+                                <th class="e20r-label header"><label for="e20r-checkin-item-name">Name</label></th>
+                                <th class="e20r-label header"><label for="e20r-checkin-startdate">Starts on</label></th>
+                                <th class="e20r-label header"><label for="e20r-checkin-enddate">Ends on</label></th>
+                                <th class="e20r-label header"><label for="e20r-checkin-maxcount">Max count</label></th>
+                                <th class="e20r-save-col hidden">Save</th>
+                                <th class="e20r-cancel-col hidden">Cancel</th>
+                                <th class="e20r-delete-col hidden">Remove</th>
+                                <th class="e20r-label header hidden"></th>
+                            </tr>
+                            <tr>
+                                <td colspan="13"><hr/></td>
+                                <!-- select for choosing the membership type to tie this check-in to -->
+                            </tr>
+                        </thead>
+                        <tbody>
+                    <?php
+                        if ( count($item_list) > 0) {
+
+                            foreach ( $item_list as $item ) {
+
+                                if ( is_null( $item->starttime ) ) {
+                                    $start = '';
+                                } else {
+                                    $start = new DateTime( $item->starttime );
+                                    $start = $start->format( 'Y-m-d' );
+                                }
+
+                                if ( is_null( $item->endtime ) ) {
+                                    $end = '';
+                                } else {
+                                    $end = new DateTime( $item->endtime );
+                                    $end = $end->format( 'Y-m-d' );
+                                }
+
+                                if ( is_object( $end ) && is_object( $start ) ) {
+
+                                    $diff = $end->diff( $start );
+
+                                    $max = $diff->format( '%a' );
+                                }
+                                else {
+                                    $max = 14; // Default max if a habit is 14 days.
+                                }
+
+                                $iId = $item->id;
+
+                                ?>
+                                <tr id="<?php echo $iId; ?>" class="check-in-inputs">
+                                    <td class="text-input">
+                                        <input type="checkbox" name="checkin-item-edit" id="edit_<?php echo $iId; ?>">
+                                    </td>
+                                    <td class="text-input">
+                                        <input type="text" id="e20r-checkin-item-id_<?php echo $iId; ?>" disabled name="e20r-checkin-item-id" size="5" value="<?php echo( ( ! empty( $item->id ) ) ? $item->id : null ); ?>">
+                                    </td>
+
+                                    <td class="text-input">
+                                        <input type="text" name="e20r-checkin-item-order" id="e20r-checkin-item-order_<?php echo $iId; ?>" disabled size="4" value="<?php echo( ( ! empty( $item->item_order ) ) ? $item->item_order : null ); ?>">
+                                    </td>
+                                    <td class="select-input">
+                                        <?php echo $programs->programSelector( false, $item->program_id ); ?>
+                                    </td>
+                                    <td class="text-input">
+                                        <input type="text" name="e20r-checkin-short-name" id="e20r-checkin-item-short-name_<?php echo $iId; ?>" disabled size="25" value="<?php echo( ( ! empty( $item->short_name ) ) ? $item->short_name : null ); ?>">
+                                    </td>
+                                    <td class="text-input">
+                                        <input type="text" name="e20r-checkin-item-name" id="e20r-checkin-item-name_<?php echo $iId; ?>" disabled size="50" value="<?php echo( ( ! empty( $item->item_name ) ) ? $item->item_name : null ); ?>">
+                                    </td>
+                                    <td class="text-input">
+                                        <input type="date" name="e20r-checkin-startdate" id="e20r-checkin-item-startdate_<?php echo $iId; ?>" disabled value="<?php echo( ( ! empty( $start ) ) ? $start : null ); ?>">
+                                    </td>
+                                    <td class="text-input">
+                                        <input type="date" name="e20r-checkin-enddate" id="e20r-checkin-item-enddate_<?php echo $iId; ?>" disabled value="<?php echo( ( ! empty( $end ) ) ? $end : null ); ?>">
+                                    </td>
+                                    <td class="text-input">
+                                        <input type="text" name="e20r-checkin-maxcount" id="e20r-checkin-item-maxcount_<?php echo $iId; ?>" disabled size="5" value="<?php echo $max; ?>" >
+                                    </td>
+
+                                    <td class="hidden select-input">
+                                        <!-- Insert membership type this program belongs to -->
+                                    </td>
+                                    <td class="hidden save-button-row" id="e20r-td-save_<?php echo $iId; ?>">
+                                        <a href="#" class="e20r-save-edit button">Save</a>
+                                    </td>
+                                    <td class="hidden cancel-button-row" id="e20r-td-cancel_<?php echo $iId; ?>">
+                                        <a href="#" class="e20r-cancel-edit button">Cancel</a>
+                                    </td>
+                                    <td class="hidden delete-button-row" id="e20r-td-delete_<?php echo $iId; ?>">
+                                        <a href="#" class="e20r-delete button">Remove</a>
+                                    </td>
+                                    <td class="hidden hidden-input">
+                                        <input type="hidden" class="hidden_id" value="<?php echo $iId; ?>">
+                                    </td>
                                 </tr>
-                                <tr>
-                                    <!-- select for choosing the membership type to tie this check-in to -->
-                                </tr>
-                            </thead>
-                            <tbody>
-                        <?php
-                            foreach ($item_list as $item ) {
+                            <?php
+                            } // foreach
+                        } // Endif
+                         else {
+                         ?>
+                             <tr>
+                                 <td colspan="13">
+                                     No Items found in the database. Please add one or more new Items by clicking the "Add New" button.
+                                 </td>
+                             </tr>
+
+                         <?php
+                         }
                         ?>
-                                <tr>
-                                    <td class="select-input"><?php echo $programs->programSelector( false, $item->program_id ); ?></td>
-                                    <td class="text-input"><input type="text" name="e20r-checkin-short_name" id="e20r_checkin_item_short_name" size="25" value="<?php echo ( ( ! empty($item->short_name) ) ? $item->short_name : null ); ?>"></td>
-                                    <td class="text-input"><input type="text" name="e20r-checkin-item_name" id="e20r_checkin_item_name" size="50" value="<?php echo ( ( ! empty($item->item_name) ) ? $item->item_name : null ); ?>" ></td>
-                                    <td class="text-input"><input type="date" name="e20r-checkin-startdate" id="e20r_checkin_item_startdate" value="<?php echo ( ( ! empty($item->startdate) ) ? $item->startdate : null ); ?>" ></td>
-                                    <td class="text-input"><input type="text" name="e20r-checkin-item_order" id="e20r_checkin_item_order" size="4" value="<?php echo ( ( ! empty($item->item_order) ) ? $item->item_order : null ); ?>" ></td>
-                                    <td class="text-input"><input type="date" name="e20r-checkin-enddate" id="e20r_checkin_item_enddate" value="<?php echo ( ( ! empty($item->enddate) ) ? $item->enddate : null ); ?>" ></td>
-                                    <td class="text-input"><input type="text" name="e20r-checkin-item_order" id="e20r_checkin_item_order" size="4" value="<?php echo ( ( ! empty($item->item_order) ) ? $item->item_order : null ); ?>" ></td>
-                                    <td class="select-input"><!-- Insert membership type this program belongs to --></td>
-                                    <td><a class="e20r-choice-button button" href="<?php echo admin_url('admin.php?page=e20r-edit-program'); ?>" target="_blank">Edit</a></td>
-                                </tr>
-                        <?php } ?>
-                                </tbody>
-                        </table>
-                    </div>
-                </form>
-        <?php } else { ?>
-            No programs found in the database. Please add one or more new programs first.
-        <?php } ?>
-            </div>
+                        </tbody>
+                        <tfoot>
+                            <tr>
+                                <td colspan="13"><hr/></td>
+                            </tr>
+                            <tr>
+                                <td colspan="2" class="add-new" style="text-align: left;"><a class="e20r-button button" id="e20r-add-new-item" href="#">Add New</a></td>
+                            </tr>
+                            <tr id="add-new-checkin-item" class="hidden">
+                                <td class="text-input">
+                                    <input type="checkbox" disabled name="edit" id="edit">
+                                </td>
+                                <td class="text-input">
+                                    <input type="text" id="e20r-checkin-item-id" name="e20r-checkin-item-id" disabled size="5" value="auto">
+                                </td>
+                                <td class="text-input">
+                                    <input type="text" name="e20r-checkin-item_order" id="e20r_checkin_item_order" size="4" value="">
+                                </td>
+                                <td class="select-input">
+                                    <?php echo $programs->programSelector( false, null ); ?>
+                                </td>
+                                <td class="text-input">
+                                    <input type="text" name="e20r-checkin-short_name" id="e20r_checkin_item_short_name" size="25" value="">
+                                </td>
+                                <td class="text-input">
+                                    <input type="text" name="e20r-checkin-item_name" id="e20r_checkin_item_name" size="35" value="">
+                                </td>
+                                <td class="text-input">
+                                    <input type="date" name="e20r-checkin-startdate" id="e20r_checkin_item_startdate" value="">
+                                </td>
+                                <td class="text-input">
+                                    <input type="date" name="e20r-checkin-enddate" id="e20r_checkin_item_enddate" value="">
+                                </td>
+                                <td class="text-input">
+                                    <input type="text" name="e20r-checkin-maxcount" id="e20r-checkin-item-maxcount" size="5" value="" >
+                                </td>
+                                <td class="select-input hidden">
+                                    <!-- Insert membership type this program belongs to -->
+                                </td>
+                                <td class="save">
+                                    <a class="e20r-button button" id="e20r-save-new-checkin-item" href="#save-new-checkin-item">Save</a>
+                                </td>
+                                <td class="cancel">
+                                    <a class="e20r-button button" id="e20r-cancel-new-checkin-item" href="#">Cancel</a>
+                                </td>
+                                <td class="hidden">
+                                    <!-- Nothing here, it's for the delete/remove button -->
+                                </td>
+                                <td class="hidden-input">
+                                    <input type="hidden" class="hidden_id" value="<?php echo $iId; ?>">
+                                </td>
+                            </tr>
+                        </tfoot>
+                    </table>
+                </div>
+            </form>
             <?php
             $html = ob_get_clean();
 
