e97248cb438c209831085500c35d432ab8c8b7bb
Thomas Sjolshagen <Thomas@eighty20results.com>
2014-10-02 11:55:42 -0400

    Enh: Added settings page & default settings. Fix: E20RPrograms class instantiated & saved as class attr Enh: Added description field to table for checkin_items. Modify registerSettingsPage() to load settings for back-end. Enh: Use settings during activation & deactivation of plugin Enh: Add indexes for checkin_id and answer_ids

--- classes/class.e20rTracker.php
+++ classes/class.e20rTracker.php
@@ -5,8 +5,21 @@ class e20rTracker {
     private $clientData;
     private $checkinData;
     private $exercises;
+    private $programInfo;
+
+    protected $settings = array();
+    protected $setting_name = 'e20r-tracker';
 
     public $tables;
+
+    public function __construct() {
+
+        // Set defaults (in case there are none saved already
+        $this->settings = array(
+            'delete_tables' => false,
+            'purge_tables' => true,
+        );
+    }
 
     public function init() {
 
@@ -14,6 +27,7 @@ class e20rTracker {
 
         $this->tables = new stdClass();
 
+        /* The database tables used by this plugin */
         $this->tables->checkin_items = $wpdb->prefix . 'e20r_checkin_items';
         $this->tables->checkin_rules = $wpdb->prefix . 'e20r_checkin_rules';
         $this->tables->checkin = $wpdb->prefix . 'e20r_checkin';
@@ -23,18 +37,18 @@ class e20rTracker {
         $this->tables->sets = $wpdb->prefix . 'e20r_sets';
         $this->tables->exercise = $wpdb->prefix . 'e20r_exercises';
 
+        /* Load required classes used by the plugin */
         $this->clientData = new S3F_clientData();
         $this->checkinData = new E20Rcheckin();
         $this->exercises = new ExercisePrograms();
         $this->programInfo = new e20rPrograms();
 
-        add_action( 'admin_menu', array( &$this, 'loadAdminPage') );
-
+        /* Load scripts & CSS */
         add_action( 'admin_enqueue_scripts', array( &$this, 'load_plotSW') );
         add_action( 'admin_enqueue_scripts', array( &$this, 'load_adminJS') );
-
         add_action( 'wp_enqueue_scripts', array( &$this, 'load_plotSW' ) );
 
+        /* AJAX call-backs */
         add_action( 'wp_ajax_get_checkinItem', array( &$this->checkinData, 'ajax_getCheckin_item' ) );
         add_action( 'wp_ajax_e20r_clientDetail', array( &$this->clientData, 'ajax_clientDetail' ) );
         add_action( 'wp_ajax_e20r_complianceData', array( &$this->clientData, 'ajax_complianceData' ) );
@@ -44,13 +58,42 @@ class e20rTracker {
 
         add_action( 'wp_ajax_save_program_info', array( &$this->programInfo, 'ajax_save_program_info' ) );
 
+        /* AJAX call-backs if user is unprivileged */
         add_action( 'wp_ajax_nopriv_e20r_clientDetail', array( &$this->clientData, 'ajaxUnprivError' ) );
         add_action( 'wp_ajax_nopriv_e20r_complianceData', array( &$this->clientData, 'ajaxUnprivError' ) );
         add_action( 'wp_ajax_nopriv_e20r_assignmentsData', array( &$this->clientData, 'ajaxUnprivError' ) );
         add_action( 'wp_ajax_nopriv_e20r_measurementsData', array( &$this->clientData, 'ajaxUnprivError' ) );
 
-        // $this->clientData = new S3F_clientData();
+        /* Load various back-end pages/settings */
+        add_action( 'admin_menu', array( &$this, 'loadAdminPage') );
+        add_action( 'admin_init', array( &$this, 'registerSettingsPage' ) );
 
+    }
+
+    public function validate( $input ) {
+
+        $valid = array();
+
+        dbg('Running validation: ' . print_r( $input, true ) );
+
+        foreach ( $input as $key => $value ) {
+
+            $valid[$key] = apply_filters( 'e20r_settings_validation_' . $key, $value );
+
+        }
+
+        /*
+         * Use add_settings_error( $title (title of setting), $errorId (text identifier), $message (Error message), 'error' (type of message) ); if needed
+         */
+
+        unset( $input ); // Free.
+
+        return $valid;
+    }
+
+    public function validate_bool( $value ) {
+
+        return ( 1 == intval( $value ) ? 1 : 0);
     }
 
     /**
@@ -63,13 +106,90 @@ class e20rTracker {
 
     function loadAdminPage() {
 
-        if (! empty($this->clientData ) ) {
+        add_options_page( 'Eighty / 20 Tracker', 'E20R Tracker', 'manage_options', 'e20r_tracker_opt_page', array( $this, 'render_settings_page' ) );
 
-            // Init the S3F Client data class
-            $this->clientData->init();
+        // $this->registerAdminPages();
 
-            $this->clientData->registerAdminPages();
-        }
+    }
+
+    public function registerSettingsPage() {
+
+        // Register any global settings for the Plugin
+        register_setting( 'e20r_options', $this->setting_name, array( $this, 'validate' ) );
+
+        add_settings_section( 'e20r_tracker_deactivate', 'Deactivation settings', array( &$this, 'render_section_text' ), 'e20r_tracker_opt_page' );
+
+        /* Add fields for the settings */
+        add_settings_field( 'e20r_tracker_purge_tables', __("Clear tables", 'e20r_tracker'), array( $this, 'render_purge_checkbox'), 'e20r_tracker_opt_page', 'e20r_tracker_deactivate');
+        add_settings_field( 'e20r_tracker_delete_tables', __("Delete tables", 'e20r_tracker'), array( $this, 'render_delete_checkbox'), 'e20r_tracker_opt_page', 'e20r_tracker_deactivate');
+
+        // $this->render_settings_page();
+
+    }
+
+    public function render_delete_checkbox() {
+
+        $options = get_option( $this->setting_name );
+        ?>
+        <input type="checkbox" name="<?php echo $this->setting_name; ?>[delete_tables]" value="1" <?php checked( 1, $options['delete_tables'] ) ?> >
+        <?php
+    }
+
+    public function render_purge_checkbox() {
+
+
+        $options = get_option( $this->setting_name );
+        ?>
+        <input type="checkbox" name="<?php echo $this->setting_name; ?>[purge_tables]" value="1" <?php checked( 1, $options['purge_tables'] ) ?> >
+    <?php
+    }
+
+    public function render_section_text() {
+
+        $html = "<p>These settings will determine the behavior of the plugin during deactivation.</p>";
+
+        echo $html;
+    }
+
+    public function render_settings_page() {
+
+        ob_start();
+        ?>
+        <div id="e20r-settings">
+            <div class="wrap">
+                <h2>Settings: Eighty / 20 Tracker</h2>
+                <form method="post" action="options.php">
+                    <?php settings_fields( 'e20r_options' ); ?>
+                    <?php do_settings_sections( 'e20r_tracker_opt_page' ); ?>
+                    <p class="submit">
+                        <input type="submit" class="button-primary" value="<?php _e('Save Changes'); ?>" />
+                    </p>
+                </form>
+            </div>
+        </div>
+    <?php
+        $html = ob_get_clean();
+
+        echo $html;
+    }
+
+    public function registerAdminPages() {
+
+        $page = add_menu_page( 'S3F Clients', __('S3F Clients','e20r_tracker'), 'manage_options', 'e20r-tracker', array( &$this->clientData, 'render_client_page' ), 'dashicons-admin-generic', '71.1' );
+//        add_submenu_page( 'e20r-tracker', __('Measurements','e20r_tracker'), __('Measurements','e20r_tracker'), 'manage-options', "e20r_tracker_measure", array( &$this,'render_measurement_page' ));
+
+//      add_submenu_page( 'e20r-tracker', __('Manage Program','e20r_tracker'), __('Add Program','e20r_tracker'), 'manage_options', "e20r-add-new-program", array( &$this,'render_new_program_page'));
+        add_submenu_page( 'e20r-tracker', __('Programs','e20r_tracker'), __('Programs','e20r_tracker'), 'manage_options', "e20r-tracker-list-programs", array( &$this->programInfo, 'render_submenu_page'));
+
+//        add_submenu_page( 'e20r-tracker', __('Manage Item','e20r_tracker'), __('Add Item','e20r_tracker'), 'manage_options', "e20-add-new-item", array( &$this,'render_new_item_page'));
+        add_submenu_page( 'e20r-tracker', __('Items','e20r_tracker'), __('Items','e20r_tracker'), 'manage_options', "e20r-tracker-list-items", array( &$this->checkinData, 'render_submenu_page'));
+        add_submenu_page( 'e20r-tracker', __('Settings','e20r_tracker'), __('Settings','e20r_tracker'), 'manage_options', "e20r-tracker-settings", array( &$this, 'registerSettingsPage'));
+
+        //add_submenu_page( 'e20r-tracker', __('Check-in Items','e20r_tracker'), __('Items','e20r_tracker'), 'manage-options', 'e20r-items', array( &$this, 'render_management_page' ) );
+
+//        add_submenu_page( 'e20r-tracker', __('Meals','e20r_tracker'), __('Meal History','e20r_tracker'), 'manage_options', "e20r_tracker_meals", array( &$this,'render_meals_page'));
+
+        // add_action( "admin_print_scripts-$page", array( 'e20rTracker', 'load_adminJS') ); // Load datepicker, etc (see apppontments+)
     }
 
     /**
@@ -155,10 +275,13 @@ class e20rTracker {
 
     }
 
-    static function e20r_tracker_activate() {
+    public function e20r_tracker_activate() {
 
         global $wpdb;
         global $e20r_db_version;
+
+        // Create settings with default values
+        update_option( $this->setting_name, $this->settings );
 
         $charset_collate = '';
 
@@ -171,6 +294,7 @@ class e20rTracker {
         }
 
         dbg("e20r_tracker_activate() - Loading table SQL");
+
 
         $programsTableSql = "
             CREATE TABLE IF NOT EXISTS {$wpdb->prefix}e20r_programs (
@@ -246,24 +370,25 @@ class e20rTracker {
                     thigh decimal(18,3) null,
                     calf decimal(18,3) null,
                     girth decimal(18,3) null,
-                    primary key  (id),
+                    primary key  ( id ),
                     key user_id ( user_id asc) )
                   {$charset_collate}
               ";
-
+        // TODO: Add item_text on admin page.
         $itemsTableSql =
             "CREATE TABLE IF NOT EXISTS {$wpdb->prefix}e20r_checkin_items (
                     id int not null auto_increment,
                     short_name varchar(20) null,
                     program_id int null,
                     item_name varchar(50) null,
+                    item_text mediumtext null,
                     startdate datetime null,
                     enddate datetime null,
                     item_order int not null default 1,
                     maxcount int null,
                     membership_level_id int not null default 0,
-                primary key  (id) ,
-                unique key shortname_UNIQUE (short_name asc) )
+                primary key  ( id ) ,
+                unique key shortname_UNIQUE ( short_name asc ) )
                 {$charset_collate}";
 
         $businessRulesSql =
@@ -271,18 +396,22 @@ class e20rTracker {
                     id int not null auto_increment,
                     checkin_id int null,
                     success_rule mediumtext null,
-                    primary key  (id),
-                    key checkin_id (checkin_id asc) )
+                    primary key  ( id ),
+                    key checkin_id ( checkin_id asc ) )
                 {$charset_collate}";
 
+        // TODO: How do you combine Assignments (flexibility, unlimited # of boxes & questions) and
         $checkinSql =
             "CREATE TABLE IF NOT EXISTS {$wpdb->prefix}e20r_checkin (
                     id int not null auto_increment,
                     user_id int null,
                     checkin_date datetime null,
-                    checkin_id int null,
-                    program_id int null, -- Uses the membership_level->ID value (unless it's nourish)
-                    primary key  (id) )
+                    checkin_item_id int null,
+                    checkedin tinyint not null default 0,
+                    answer_id int null,
+                    primary key  (id),
+                      key checkin_item_id ( checkin_item_id asc ),
+                      key answer_id ( answer_id asc ) )
                 {$charset_collate}";
 
         require_once( ABSPATH . "wp-admin/includes/upgrade.php" );
@@ -302,14 +431,14 @@ class e20rTracker {
         flush_rewrite_rules();
     }
 
-    static function e20r_tracker_deactivate() {
+    public function e20r_tracker_deactivate() {
 
         global $wpdb;
         global $e20r_db_version;
 
-        $deleteTables = get_option( 'e20r_clean_tables', true );
+        $options = get_option( $this->setting_name );
 
-        dbg("Loading table SQL");
+        dbg("Deactivation options: " . print_r( $options, true ) );
 
         $tables = array(
             $wpdb->prefix . 'e20r_checkin_items',
@@ -322,15 +451,27 @@ class e20rTracker {
             $wpdb->prefix . 'e20r_exercises',
         );
 
-        if ( $deleteTables !== false) {
 
-            foreach ( $tables as $tblName ) {
+        foreach ( $tables as $tblName ) {
+
+            if ( $options['purge_tables'] == 1 ) {
+
+                dbg("e20r_tracker_deactivate() - Truncating {$tblName}" );
+                $sql = "TRUNCATE TABLE {$tblName}";
+                $wpdb->query( $sql );
+            }
+
+            if ( $options['delete_tables'] == 1 ) {
 
                 dbg( "e20r_tracker_deactivate() - {$tblName} being dropped" );
 
                 $sql = "DROP TABLE IF EXISTS {$tblName}";
                 $wpdb->query( $sql );
             }
+
         }
+
+        // Remove existing options
+        delete_option( $this->setting_name );
     }
 }
