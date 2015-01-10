e394a14b95463b04d48b8ab6a84bf3dd278adde9
Thomas Sjolshagen <Thomas@eighty20results.com>
2015-01-10 16:26:53 -0500

    Support for e20rSettings base class Fix formatting of viewSettingsBox() metabox Add support for tying multiple programs to a check-in item Add license

--- classes/views/class.e20rCheckinView.php
+++ classes/views/class.e20rCheckinView.php
@@ -1,9 +1,10 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: sjolshag
- * Date: 12/15/14
- * Time: 1:20 PM
+ * Created by Eighty / 20 Results, owned by Wicked Strong Chicks, LLC.
+ * Developer: Thomas Sjolshagen <thomas@eigthy20results.com>
+ *
+ * License Information:
+ *  the GPL v2 license(?)
  */
 
 class e20rCheckinView {
@@ -18,21 +19,20 @@ class e20rCheckinView {
 
     public function viewSettingsBox( $checkinData, $programs ) {
 
-        dbg( "e20rCheckinView::viewCheckinSettingsBox() - Supplied data: " . print_r( $checkinData, true ) );
+        dbg( "e20rCheckinView::viewSettingsBox() - Supplied data: " . print_r( $checkinData, true ) );
         ?>
         <form action="" method="post">
             <?php wp_nonce_field( 'e20r-tracker-data', 'e20r-tracker-checkin-settings' ); ?>
             <div class="e20r-editform">
                 <input type="hidden" name="hidden-e20r-checkin-id" id="hidden-e20r-checkin-id"
                        value="<?php echo( ( ! empty( $checkinData ) ) ? $checkinData->ID : 0 ); ?>">
-                <table id="e20r-checkin-settings">
+                <table id="e20r-checkin-settings wp-list-table widefat fixed">
                     <thead>
                     <tr>
                         <th class="e20r-label header"><label for="e20r-checkin-starttime">Starts on</label></th>
                         <th class="e20r-label header"><label for="e20r-checkin-endtime">Ends on</label></th>
                         <th class="e20r-label header"><label for="e20r-checkin-number_days">Max # Check-ins</label></th>
-                        <th class="e20r-label header"><label for="e20r-checkin-shortname">Shortname</label></th>
-                        <th class="e20r-label header"><label for="e20r-checkin-dripfeed">Program</label></th>
+                        <th class="e20r-label header"><label for="e20r-checkin-program_id">Program</label></th>
                     </tr>
                     <tr>
                         <td colspan="5">
@@ -66,29 +66,23 @@ class e20rCheckinView {
                     ?>
                     <tr id="<?php echo $checkinData->ID; ?>" class="checkin-inputs">
                         <td class="text-input">
-                            <input type="date" id="e20r-checkin-starttime" name="e20r-checkin-startdate"
-                                   value="<?php echo $start; ?>">
+                            <input type="date" id="e20r-checkin-startdate" name="e20r-checkin-startdate" value="<?php echo $start; ?>">
                         </td>
                         <td class="text-input">
-                            <input type="date" id="e20r-checkin-endtime" name="e20r-checkin-enddate"
-                                   value="<?php echo $end; ?>">
+                            <input type="date" id="e20r-checkin-enddate" name="e20r-checkin-enddate" value="<?php echo $end; ?>">
                         </td>
                         <td class="text-input">
-                            <input type="date" id="e20r-checkin-maxcount" name="e20r-checkin-maxcount"
-                                   value="<?php echo $checkinData->maxcount; ?>">
-                        </td>
-                        <td class="text-input">
-                            <input type="text" id="e20r-checkin-shortname" name="e20r-checkin-short_name" size="25"
-                                   value="<?php echo( ( ! empty( $checkinData->short_name ) ) ? $checkinData->short_name : null ); ?>">
+                            <input type="number" id="e20r-checkin-maxcount" name="e20r-checkin-maxcount" value="<?php echo $checkinData->maxcount; ?>">
                         </td>
                         <td>
-                            <select class="select2-container" id="e20r-checkin-program" name="e20r-checkin-program">
+                            <select class="select2-container" id="e20r-checkin-program_id" name="e20r-checkin-program_id[]" multiple>
                                 <option value="0">Not configured</option>
                                 <?php
+                                foreach ( $programs as $pgm ) {
 
-                                foreach ( $programs as $pgm ) { ?>
+                                    $selected = ( in_array( $pgm->ID, $checkinData->program_id ) ? ' selected="selected" ' : null); ?>
                                     <option
-                                        value="<?php echo $pgm->ID; ?>"<?php selected( $checkinData->program_id, $pgm->ID ); ?>><?php echo esc_textarea( $pgm->post_title ); ?>
+                                        value="<?php echo $pgm->ID; ?>"<?php echo $selected; ?>><?php echo esc_textarea( $pgm->post_title ); ?>
                                         (#<?php echo $pgm->ID; ?>)
                                     </option>
                                 <?php } ?>
@@ -101,7 +95,7 @@ class e20rCheckinView {
                                 }
                             </style>
                             <script>
-                                jQuery('#e20r-checkin-program').select2();
+                                jQuery('#e20r-checkin-program_id').select2();
                             </script>
                         </td>
                     </tr>
