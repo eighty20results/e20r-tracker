e3c8810d3c2e480793d94986f224b44b75a408cd
Thomas Sjolshagen <Thomas@eighty20results.com>
2015-01-14 05:07:21 -0500

    Renamed Workout to Activity

--- classes/views/class.e20rCheckinView.php
+++ classes/views/class.e20rCheckinView.php
@@ -154,7 +154,7 @@ class e20rCheckinView extends e20rSettingsView {
                                 <option value="0"><?php _e("Not configured", "e20rtracker"); ?></option>
                                 <option value="1"><?php _e("Action", "e20rtracker"); ?></option>
                                 <option value="2"><?php _e("Assignment", "e20rtracker"); ?></option>
-                                <option value="3"><?php _e("Workout", "e20rtracker"); ?></option>
+                                <option value="3"><?php _e("Activity", "e20rtracker"); ?></option>
                                 <option value="4"><?php _e("Survey", "e20rtracker"); ?></option>
                             </select>
                         </td>
