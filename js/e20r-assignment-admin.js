42844118ac63b01f04ae658fb006cc48fa011c31
Thomas Sjolshagen <Thomas@eighty20results.com>
2015-02-11 10:52:41 -0500

    Add skeleton assignment admin script file

---
+++ js/e20r-assignment-admin.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Eighty / 20 Results, owned by Wicked Strong Chicks, LLC.
+ * Developer: Thomas Sjolshagen <thomas@eigthy20results.com>
+ *
+ * License Information:
+ *  the GPL v2 license(?)
+ */
+
+function e20r_assignmentEdit( assignmentId ) {
+
+};
+
+function e20r_assignmentRemove( assignmentId ) {
+
+};
+
+function e20r_assignmentSave() {
+
+}
