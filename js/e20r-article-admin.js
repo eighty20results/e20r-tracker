a889a888757fc6bc631c23f80d8707faa785ce47
Thomas Sjolshagen <Thomas@eighty20results.com>
2015-02-12 17:13:03 -0500

    Fix: jQuery load error

--- js/e20r-article-admin.js
+++ js/e20r-article-admin.js
@@ -9,18 +9,19 @@
 jQuery(document).ready( function(){
 
     jQuery('#e20r-assignments-id').select2();
+    jQuery('#e20r-article-post_id').select2();
 
-    $(document).on('change', '#e20r-article-post_id', function() {
+    jQuery(document).on('change', '#e20r-article-post_id', function() {
 
-        $.ajax({
+        jQuery.ajax({
             url: ajaxurl,
             type: 'POST',
             timeout: 10000,
             dataType: 'JSON',
             data: {
                 action: 'getDelayValue',
-                'post_ID': $('#e20r-article-post_id').find('option:selected').val(),
-                'e20r-tracker-article-settings-nonce': $('#e20r-tracker-article-settings-nonce').val()
+                'post_ID': jQuery('#e20r-article-post_id').find('option:selected').val(),
+                'e20r-tracker-article-settings-nonce': jQuery('#e20r-tracker-article-settings-nonce').val()
             },
             success: function( $response ) {
 
@@ -34,7 +35,7 @@ jQuery(document).ready( function(){
                 if ( $response.data.delay > 0 ) {
 
                     console.log("Got delay value from back-end: " + $response.data.delay);
-                    $('#e20r-article-release_day').val($response.data.delay);
+                    jQuery('#e20r-article-release_day').val($response.data.delay);
                 }
             },
             error: function( $response, $errString, $errType ) {
@@ -43,3 +44,71 @@ jQuery(document).ready( function(){
         });
     });
 });
+
+function e20r_assignmentEdit( assignmentId ) {
+
+    console.log("AssignmentId to edit: " + assignmentId );
+};
+
+function e20r_assignmentRemove( assignmentId ) {
+
+    console.log("AssignmentId to remove: " + assignmentId );
+};
+
+function e20r_assignmentSave() {
+
+    console.log("Save assignment to article" );
+
+    var saveBtn = jQuery('#e20r-article-assignment-save');
+
+    if ('' == jQuery('#e20r-assignments-id').val() || undefined != saveBtn.attr('disabled'))
+        return false; //already processing, ignore this request
+
+    // Disable save button
+    saveBtn.attr('disabled', 'disabled');
+    saveBtn.html(e20r_tracker.lang.saving);
+
+    //pass field values to AJAX service and refresh table above - Timeout is 5 seconds
+    jQuery.ajax({
+        url: e20r_tracker.ajaxurl,
+        type:'POST',
+        timeout:5000,
+        dataType: 'JSON',
+        data: {
+            action: 'e20r_addAssignment',
+            'e20r-tracker-article-settings-nonce': jQuery('#e20r-tracker-article-settings-nonce').val(),
+            'e20r-assignment-id': jQuery('#e20r-add-assignment-id').find("option:selected").val(),
+            'e20r-assignment-order_num': jQuery('#e20r-add-assignment-order_num').val(),
+            'e20r-article-id': jQuery('#post_ID').val()
+
+            // 'e20r-tracker-assignment-settings': jQuery('#e20r-tracker-assignment-settings').val()
+        },
+        error: function($data,  $errString, $errType){
+            console.log("error() - Returned data: " + $data.success + ", data: ", $data.data);
+            console.log("Error String: " + $errString + " and errorType: " + $errType);
+
+            if ( $data.data ) {
+                alert($data.data);
+                // pmpro_seq_setErroMsg($data.data);
+            }
+        },
+        success: function($data){
+            console.log("success() - Returned data: ", $data);
+
+            if ($data.data) {
+                console.log('Entry added to sequence & refreshing metabox content');
+                jQuery('#e20r-assignment-settings').html($data.data);
+            } else {
+                console.log('No HTML returned???');
+            }
+
+        },
+        complete: function($data) {
+
+            // Re-enable save button
+            saveBtn.html(e20r_tracker.lang.save);
+            saveBtn.removeAttr('disabled');
+
+        }
+    });
+}
