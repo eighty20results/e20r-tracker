30365da1b610c6595bfdc998d49cf0078feaea4f
Thomas Sjolshagen <Thomas@eighty20results.com>
2015-02-20 06:45:44 -0500

    Enh: Adding skeleton JavaScript file for the e20r_workout post type.

---
+++ js/e20r-workout-admin.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Eighty / 20 Results, owned by Wicked Strong Chicks, LLC.
+ * Developer: Thomas Sjolshagen <thomas@eigthy20results.com>
+ *
+ * License Information:
+ *  the GPL v2 license(?)
+ */
+
+jQuery(document).ready( function(){
+
+    jQuery('#e20r-workout-id').select2();
+});
