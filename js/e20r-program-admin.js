093ad4676b850beee55f54314b5c01651ef2207f
Thomas Sjolshagen <Thomas@eighty20results.com>
2015-03-25 11:52:14 -0400

    Enh: Split out javascript to its own minifiable file.

---
+++ js/e20r-program-admin.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Eighty / 20 Results, owned by Wicked Strong Chicks, LLC.
+ * Developer: Thomas Sjolshagen <thomas@eigthy20results.com>
+ *
+ * License Information:
+ *  the GPL v2 license(?)
+ */
+
+jQuery(document).ready( function(){
+
+    jQuery("#e20r-program-groups").select2();
+    jQuery('#e20r-program-sequences').select2();
+    jQuery('#e20r-program-activity_sequences').select2();
+});
+
+
