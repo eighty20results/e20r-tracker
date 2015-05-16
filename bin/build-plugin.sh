be1b7c898d8ed318d856cffb58f61ca9140a53a9
Thomas Sjolshagen <Thomas@eighty20results.com>
2015-05-16 12:02:42 -0400

    Fix: Clean up existing kits for the version being built Fix: Clean up after ourselves.

--- bin/build-plugin.sh
+++ bin/build-plugin.sh
@@ -6,13 +6,24 @@ plugin_path="${short_name}"
 version=$(grep Version ../${short_name}.php | awk '{print $2}')
 src_path="../"
 dst_path="../build/${plugin_path}-${version}/"
+kit_path="../build/kits"
+kit_name="${kit_path}/${short_name}-${version}.zip"
 
-mkdir -p ${dst_path} 
+mkdir -p ${kit_path}
+mkdir -p ${dst_path}
+
+if [ -f  ${kit_name} ]
+then
+    echo "Kit is already present. Cleaning up"
+    rm -rf ${dst_path}
+    rm -f ${kit_name}
+fi
 
 for p in ${include[@]}; do
 	cp -R ${src_path}${p} ${dst_path}
 done
 
 cd ${dst_path}/..
-zip -r ${short_name}-${version}.zip ${short_name}-${version}
+zip -r ${kit_name} ${short_name}-${version}
+rm -rf ${dst_path}
 
