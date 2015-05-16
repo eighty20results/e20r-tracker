87cfb20dc05291378d80d34617caa68c88e26f57
Thomas Sjolshagen <Thomas@eighty20results.com>
2015-05-16 19:59:07 -0400

    Fix: Hide related videos from YouTube after playback is complete

--- js/e20r-exercise.js
+++ js/e20r-exercise.js
@@ -53,7 +53,7 @@ function e20rIframe() {
 
     var iframe = document.createElement("iframe");
 
-    iframe.setAttribute("src", "//www.youtube.com/embed/" + this.parentNode.dataset.id + "?autoplay=1&autohide=2&border=0&wmode=opaque&enablejsapi=1&controls=0&showinfo=0");
+    iframe.setAttribute("src", "//www.youtube.com/embed/" + this.parentNode.dataset.id + "?autoplay=1&autohide=2&border=0&wmode=opaque&enablejsapi=1&controls=0&showinfo=0&rel=0");
 
     iframe.setAttribute("frameborder", "0");
     iframe.setAttribute("id", "youtube-iframe");
