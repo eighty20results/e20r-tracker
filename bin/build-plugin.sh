6d44e268e2c44fe2369757f070f21b8f43a445b8
Thomas Sjolshagen <Thomas@eighty20results.com>
2015-05-16 11:08:28 -0400

    Clean: Fix variable names

--- bin/build-plugin.sh
+++ bin/build-plugin.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-INCLUDE=(classes css images js languages e20r-tracker.php README.txt)
+include=(classes css images js languages e20r-tracker.php README.txt)
 short_name="e20r-tracker"
 plugin_path="${short_name}"
 version=$(grep Version ../${short_name}.php | awk '{print $2}')
@@ -9,7 +9,7 @@ dst_path="../build/${plugin_path}-${version}/"
 
 mkdir -p ${dst_path} 
 
-for p in ${INCLUDE[@]}; do
+for p in ${include[@]}; do
 	cp -R ${src_path}${p} ${dst_path}
 done
 
