8603bd131ed73713e57ed06017aa09c41fe6e591
Thomas Sjolshagen <thomas@eighty20results.com>
2015-07-31 12:31:16 -0400

    Fix SQL for update function

--- e20r_db_update.php
+++ e20r_db_update.php
@@ -1,41 +1,53 @@
 <?php
 
-if ( !function_exists( "update_db_to_2" ) ) {
+if ( !function_exists( "update_db_to_5" ) ) {
 
-    function update_db_to_2() {
+    function update_db_to_5() {
 
-        dbg("update_db_to_2() - Upgrading database for e20r-tracker plugin to version 2");
+        dbg("update_db_to_5() - Upgrading database for e20r-tracker plugin to version " . E20R_DB_VERSION );
 
         global $wpdb;
         global $e20rTracker;
 
+        $error = false;
         $update_enums = array();
 
         $add_enum = "
-                ALTER TABLE {$wpdb}e20_assignments
-                CHANGE `field_type` `field_type` enum('textbox','input','checkbox','radio','button','yesno','survey', 'multichoice', 'rank')";
+                ALTER TABLE {$wpdb->prefix}e20r_assignments
+                CHANGE field_type field_type enum('textbox','input','checkbox','radio','button','yesno','survey', 'multichoice', 'rank')";
 
         $update_enums[] = "
-                UPDATE TABLE {$wpdb}e20r_assignments
-                SET `field_type` = 'rank'
-                WHERE `field_type` = 'survey'";
+                UPDATE {$wpdb->prefix}e20r_assignments
+                SET field_type = 'rank'
+                WHERE field_type = 'survey'";
 
         $remove_enum = "
-                ALTER TABLE {$wpdb}e20r_assignments
-                CHANGE `field_type` `field_type` enum('button', 'input', 'textbox', 'checkbox', 'multichoice', 'rank', 'yesno')}";
+                ALTER TABLE {$wpdb->prefix}e20r_assignments
+                CHANGE field_type field_type enum('button', 'input', 'textbox', 'checkbox', 'multichoice', 'rank', 'yesno')";
 
-        $wpdb->query( $add_enum );
-        dbg(" Testing whether last SQL statement resulted in error: " . $wpdb->print_error() );
+        if  ( false === $wpdb->query( $add_enum ) ) {
+            $error = true;
+            dbg("SQL statement resulted in error: " . $wpdb->print_error() );
+            dbg( $add_enum );
+        }
 
         foreach( $update_enums as $upd ) {
 
-            $wpdb->query( $upd );
-            dbg(" Testing whether last SQL statement resulted in error: " . $wpdb->print_error() );
+            if  ( false === $wpdb->query( $upd ) ) {
+                $error = true;
+                dbg("SQL statement resulted in error: " . $wpdb->print_error());
+                dbg($upd);
+            }
         }
 
-        $wpdb->query( $remove_enum );
-        dbg(" Testing whether last SQL statement resulted in error: " . $wpdb->print_error() );
+        if  ( false === $wpdb->query( $remove_enum ) ) {
+            $error = true;
+            dbg("SQL statement resulted in error: " . $wpdb->print_error());
+            dbg($remove_enum);
+        }
 
-        $e20rTracker->updateSetting( 'e20r_db_version', 2 );
+        if (! $error ) {
+            $e20rTracker->updateSetting( 'e20r_db_version', 5 );
+        }
     }
-} // End of function_exists('update_db_to_2')
+} // End of function_exists('update_db_to_4')
