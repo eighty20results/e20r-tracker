3561e6861f8af21d34e4903b21c07f236401f1d3
Thomas Sjolshagen <thomas@eighty20results.com>
2015-07-31 12:30:24 -0400

    Rename '*assignment-survey*' identifiers/classes to '*assignment-ranking*' Init select2 if multichoice field is present on page/in post.

--- js/e20r-assignments.js
+++ js/e20r-assignments.js
@@ -17,80 +17,87 @@ function e20rAssignmentsConfigureSurveyFields(){
 
 function setSurveyState( me ) {
     var elem = jQuery( me ),
-        input = elem.is( 'td.e20r-assignment-survey-question-choice' ) ? elem.find( 'input' ) : elem;
+        input = elem.is( 'td.e20r-assignment-ranking-question-choice' ) ? elem.find( 'input' ) : elem;
 
     if( input.is( ':disabled' ) )
         return false;
 
     input.prop( 'checked', true );
-    input.closest( 'tr' ).find( '.e20r-assignment-survey-question-choice-selected' ).removeClass( 'e20r-assignment-survey-question-choice-selected' );
-    input.parent().addClass( 'e20r-assignment-survey-question-choice-selected' );
+    input.closest( 'tr' ).find( '.e20r-assignment-ranking-question-choice-selected' ).removeClass( 'e20r-assignment-ranking-question-choice-selected' );
+    input.parent().addClass( 'e20r-assignment-ranking-question-choice-selected' );
     input.focus().change();
 }
 
 jQuery(document).ready(function() {
 
+    var $multichoice = jQuery('select.e20r-select2-assignment-options');
+
+    if ( $multichoice.length ) {
+
+        $multichoice.select2();
+    }
+
     console.log("Loading Assignment Survey processing");
 
-    if (jQuery( "table.e20r-assignment-survey-question").length > 0) {
+    if (jQuery( "table.e20r-assignment-ranking-question").length > 0) {
 
-        console.log("Found a survey question in the assignment");
+        console.log("Found a Ranking request in the assignment");
 
         console.log("Processing checkboxes and radio inputs");
 
-        jQuery('td.e20r-assignment-survey-question-choice').find('input[type="radio"], input[type="checkbox"]').each(function() {
+        jQuery('td.e20r-assignment-ranking-question-choice').find('input[type="radio"], input[type="checkbox"]').each(function() {
 
             var $elem = jQuery(this);
 
             if ( $elem.is(':checked') ) {
-                $elem.parent().addClass( 'e20r-assignment-survey-question-choice-selected' );
+                $elem.parent().addClass( 'e20r-assignment-ranking-question-choice-selected' );
             }
 
         });
 
-        jQuery( "table.e20r-assignment-survey-question" ).find( 'td.e20r-assignment-survey-question-choice, input[type="radio"], input[type="checkbox"]' ).on('click', function(e) {
+        jQuery( "table.e20r-assignment-ranking-question" ).find( 'td.e20r-assignment-ranking-question-choice, input[type="radio"], input[type="checkbox"]' ).on('click', function(e) {
 
             console.log("Found a survey choice in the survey question");
 
             setSurveyState( this );
             /*
             var elem = jQuery( this ),
-                input = elem.is( 'td.e20r-assignment-survey-question-choice' ) ? elem.find( 'input' ) : elem;
+                input = elem.is( 'td.e20r-assignment-ranking-question-choice' ) ? elem.find( 'input' ) : elem;
 
             if( input.is( ':disabled' ) )
                 return false;
 
             input.prop( 'checked', true );
-            input.closest( 'tr' ).find( '.e20r-assignment-survey-question-choice-selected' ).removeClass( 'e20r-assignment-survey-question-choice-selected' );
-            input.parent().addClass( 'e20r-assignment-survey-question-choice-selected' );
+            input.closest( 'tr' ).find( '.e20r-assignment-ranking-question-choice-selected' ).removeClass( 'e20r-assignment-survey-question-choice-selected' );
+            input.parent().addClass( 'e20r-assignment-ranking-question-choice-selected' );
             input.focus().change();
             */
         });
 
         // add a hover state
-        jQuery("table.e20r-assignment-survey-question td").on( 'hover', function(e){
+        jQuery("table.e20r-assignment-ranking-question td").on( 'hover', function(e){
 
-            console.log("User is hovering above survey option. Changing color");
+            console.log("User is hovering above ranking option. Changing color");
 
-            if (jQuery(e.target).is("td.e20r-assignment-survey-question-choice-label") || jQuery(this).find("input").is(':disabled')) {
+            if (jQuery(e.target).is("td.e20r-assignment-ranking-question-choice-label") || jQuery(this).find("input").is(':disabled')) {
                 return false;
             } else {
-                jQuery(this).addClass("e20r-assignment-survey-question-choice-hover");
+                jQuery(this).addClass("e20r-assignment-ranking-question-choice-hover");
             }
 
         }, function(e){
-            if (jQuery(e.target).is("td.e20r-assignment-survey-question-choice-label") || jQuery(this).find("input").is(':disabled')){
+            if (jQuery(e.target).is("td.e20r-assignment-ranking-question-choice-label") || jQuery(this).find("input").is(':disabled')){
                 return false;
             } else {
-                jQuery(this).removeClass("e20r-assignment-survey-question-choice-hover");
+                jQuery(this).removeClass("e20r-assignment-ranking-question-choice-hover");
             }
 
         });
 
         jQuery( 'table.e20r-assignment-survey-question-choice input[type="radio"], input[type="checkbox"]' ).on( 'focus', function() {
-            jQuery( this ).parent().addClass( 'e20r-assignment-survey-question-choice-hover' );
+            jQuery( this ).parent().addClass( 'e20r-assignment-ranking-question-choice-hover' );
         } ).on( 'blur', function() {
-            jQuery( this ).parent().removeClass( 'e20r-assignment-survey-question-choice-hover' );
+            jQuery( this ).parent().removeClass( 'e20r-assignment-ranking-question-choice-hover' );
         } );
 
     }
