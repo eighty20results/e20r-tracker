3f4e7bb66de3f6afd918c581e7af69dcd158f7f0
Thomas Sjolshagen <thomas@eighty20results.com>
2015-07-31 15:16:16 -0400

    Update to start on work to support building change log

--- bin/build-plugin.sh
+++ bin/build-plugin.sh
@@ -3,6 +3,14 @@
 include=(classes css images js languages e20r-tracker.php README.txt)
 short_name="e20r-tracker"
 plugin_path="${short_name}"
+readme_path="../build_readmes/"
+changelog_source=${readme_path}current.txt
+meta_log_source=${readme_path}existing_json.txt
+readme_source=${readme_path}existing_readme.txt
+json_template="metadata.json.template"
+readme_template="README.txt.template"
+readme_txt="README.txt"
+readme_json="metadata.json"
 metadata="../metadata.json"
 version=$(egrep "^Version:" ../${short_name}.php | awk '{print $2}')
 src_path="../"
@@ -21,6 +29,27 @@ then
     rm -rf ${dst_path}
     rm -f ${kit_name}
 fi
+
+#if [[ -f ${readme_path}${json_template} ]]
+#then
+#    echo "Building metadata.json file"
+#    meta_log=$(sed -e"s/\"/\'/g" -e"s/.*/\<li\>&\<\/li\>/" ${changelog_source} )
+#    #history=$(cat ${meta_log_source})
+#    to_file="\<h3\>${version}\<\/h3\>\<ol\>${meta_log}\<\/ol\>$(cat ${meta_log_source})"
+#    sed -e "s/\[JSON_LOG\]/$to_file" -e "s/\[VERSION\]/$version" ${json_template} > ${metadata}
+#fi
+
+#if [[ -f ${readme_path}${readme_template} ]]
+#then
+#    echo "Building metadata.json file"
+#    meta_log=$(sed -e"s/\"/\'/g" -e"s/.*/\*\ &/" ${changelog_source} )
+#    #history=$(cat ${readme_source})
+#    header="== ${version} =="
+#    content="${meta_log}"
+#    new=$(cat ${readme_source})
+#    all="${header}\n\n{$content}${new}"
+#    sed -e "s/\[MARKDOWN_LOG\]/${all}}" -e "s/\[VERSION\]/${version}/" ${readme_template} > ${readme_txt}
+#fi
 
 for p in ${include[@]}; do
 	cp -R ${src_path}${p} ${dst_path}
