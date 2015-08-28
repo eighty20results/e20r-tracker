803a82c21045db1bc490c7d5b0e943bf8e23ccfa
Thomas Sjolshagen <thomas@eighty20results.com>
2015-08-28 14:57:53 -0600

    Reformat date info (excludes year) for when post was released (for the current user ID) Remove author link

--- classes/controllers/class.e20rArticle.php
+++ classes/controllers/class.e20rArticle.php
@@ -1227,22 +1227,20 @@ class e20rArticle extends e20rSettings {
         ob_start(); ?>
         <article class="e20r-article-lesson">
             <div class="e20r-article-lesson-header clear-after">
-                <span class="e20r-article-lesson-date">
-                    <?php
-                    $when = $e20rTracker->getDateFromDelay( $currentArticle->release_day, $current_user->ID );
-                    echo date_i18n( get_option('date_format'), strtotime( $when ) );
-                    ?>
-                </span>
                 <span class="e20r-article-lesson-title">
                     <h2><?php echo apply_filters( 'the_title', $post->post_title ); ?></h2>
                 </span>
-                <span class="e20r-article-lesson-author">Written by: <?php the_author_link(); ?></span>
+                <span class="e20r-article-lesson-date">
+                    <?php
+                    $when = $e20rTracker->getDateFromDelay( ($currentArticle->release_day - 1), $current_user->ID );
+                    echo date_i18n( "M jS", strtotime( $when ) );
+                    ?>
+                </span>
             </div>
             <div class="e20r-article-lesson">
                 <?php
                 if ( $reading_time ) { ?>
-                    <div
-                        class="e20r-article-lesson-readingtime"><?php echo $this->reading_time($post->post_content); ?></div><?php
+                    <div class="e20r-article-lesson-readingtime"><?php echo $this->reading_time($post->post_content); ?></div><br/><?php
                 } ?>
                 <?php echo apply_filters( 'the_content', $post->post_content ); ?>
             </div>
