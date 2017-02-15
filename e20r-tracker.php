aa6ad9c2b3229199315007b50d4151fbec754b67
Thomas Sjolshagen <thomas@eighty20results.com>
2017-02-15 08:36:38 -0700

    Update version number (1.5.68)

--- e20r-tracker.php
+++ e20r-tracker.php
@@ -3,7 +3,7 @@
 Plugin Name: E20R Tracker
 Plugin URI: http://eighty20results.com/e20r-tracker
 Description: Track Coaching Activities
-Version: 1.5.67
+Version: 1.5.68
 Author: Wicked Strong Chicks, LLC <info@eighty20results.com>
 Author URI: http://eighty20results.com/thomas-sjolshagen
 Text Domain: e20rtracker
@@ -33,7 +33,7 @@ if ( !defined( 'ABSPATH' ) ) {
 	die( "Sorry, you are not allowed to access this page directly." );
 }
 
-define('E20R_VERSION', '1.5.67');
+define('E20R_VERSION', '1.5.68');
 define('E20R_RUN_UNSERIALIZE', 0); // 0 == Do NOT Run, 1 == Run
 define('E20R_DB_VERSION', '11');
 define('E20R_NEW_DB_VERSION','9');
