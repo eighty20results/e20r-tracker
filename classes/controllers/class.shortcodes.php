e57826b26f14ad682133bc47bf922a98f82e6f1e
Thomas Sjolshagen <thomas@eighty20results.com>
2018-09-04 11:46:28 -0600

    ENHANCEMENT: Loading refactored shortcode processing from a single source

--- classes/controllers/class.shortcodes.php
+++ classes/controllers/class.shortcodes.php
@@ -1,47 +1,43 @@
 <?php
 /**
- * The E20R Tracker Plugin – a coaching client management plugin for WordPress. Tracks client training, habits, educational reminders, etc.
- * Copyright (c) 2018, Wicked Strong Chicks, LLC
+ * Copyright (c) 2018. - Eighty / 20 Results by Wicked Strong Chicks.
+ *  ALL RIGHTS RESERVED
  *
- * The E20R Tracker Plugin is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License or (at your option) any later version.
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * You can contact us at info@eighty20results.com
- *
- *
+ *  You can contact us at mailto:info@eighty20results.com
  */
 
 namespace E20R\Tracker\Controllers;
 
 
+use E20R\Tracker\Controllers\Shortcodes\Activity;
+use E20R\Tracker\Controllers\Shortcodes\Activity_Archive;
+use E20R\Tracker\Controllers\Shortcodes\Article_Archive;
+use E20R\Tracker\Controllers\Shortcodes\Article_Summary;
+use E20R\Tracker\Controllers\Shortcodes\Client_Overview;
+use E20R\Tracker\Controllers\Shortcodes\Daily_Progress;
 use E20R\Tracker\Controllers\Shortcodes\Dashboard_Button;
+use E20R\Tracker\Controllers\Shortcodes\Profile;
+use E20R\Tracker\Controllers\Shortcodes\Progress_Overview;
+use E20R\Tracker\Controllers\Shortcodes\Weekly_Progress;
 
 class Shortcodes {
 	
 	private static $instance = null;
 	
 	private function __construct() {
-	}
-	
-	/**
-	 * Load Shortcode hooks
-	 */
-	public function loadHooks() {
-		
-		add_shortcode( 'e20r_tracker_dashboard_button', array( Dashboard_Button::getInstance(), 'loadShortcode' ) );
-		add_shortcode( 'weekly_progress', array( Measurements::getInstance(), 'shortcodeWeeklyProgress' ) );
-		add_shortcode( 'progress_overview', array( Measurements::getInstance(), 'shortcodeProgressOverview') );
-		add_shortcode( 'daily_progress', array( Action::getInstance(), 'shortcode_dailyProgress' ) );
-		add_shortcode( 'e20r_activity', array( Workout::getInstance(), 'shortcode_activity' ) );
-		add_shortcode( 'e20r_activity_archive', array( Workout::getInstance(), 'shortcode_act_archive' ) );
-		add_shortcode( 'e20r_exercise', array( Exercise::getInstance(), 'shortcode_exercise' ) );
-		add_shortcode( 'e20r_profile', array( Client::getInstance(), 'shortcode_clientProfile' ) );
-		add_shortcode( 'e20r_client_overview', array( Client::getInstance(), 'shortcode_clientList') );
-		add_shortcode( 'e20r_article_summary', array( Article::getInstance(), 'shortcode_article_summary') );
-		add_shortcode( 'e20r_article_archive', array( Article::getInstance(), 'article_archive_shortcode') );
 	}
 	
 	/**
@@ -54,5 +50,23 @@ class Shortcodes {
 		}
 		
 		return self::$instance;
+	}
+	
+	/**
+	 * Load Shortcode hooks
+	 */
+	public function loadHooks() {
+		
+		add_shortcode( 'e20r_tracker_dashboard_button', array( Dashboard_Button::getInstance(), 'loadShortcode' ) );
+		add_shortcode( 'weekly_progress', array( Weekly_Progress::getInstance(), 'loadShortcode' ) );
+		add_shortcode( 'progress_overview', array( Progress_Overview::getInstance(), 'loadShortcode' ) );
+		add_shortcode( 'daily_progress', array( Daily_Progress::getInstance(), 'loadShortcode' ) );
+		add_shortcode( 'e20r_activity', array( Activity::getInstance(), 'loadShortcode' ) );
+		add_shortcode( 'e20r_activity_archive', array( Activity_Archive::getInstance(), 'loadShortCode' ) );
+		add_shortcode( 'e20r_exercise', array( \E20R\Tracker\Controllers\Shortcodes\Exercise::getInstance(), 'shortcode_exercise' ) );
+		add_shortcode( 'e20r_profile', array( Profile::getInstance(), 'loadShortcode' ) );
+		add_shortcode( 'e20r_client_overview', array( Client_Overview::getInstance(), 'loadShortcode' ) );
+		add_shortcode( 'e20r_article_summary', array( Article_Summary::getInstance(), 'loadShortcode' ) );
+		add_shortcode( 'e20r_article_archive', array( Article_Archive::getInstance(), 'loadShortcode' ) );
 	}
 }
